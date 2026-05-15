6988ebd910048520c3516d9e2e6f3d9b7ca6bb23
Mobb autofixer <git@mobb.ai>
2026-05-15 12:00:28 +0000

    mobb fix commit: f5a2188d-4306-412d-8bde-0ed7ec338410

--- src/main/java/org/owasp/webgoat/lessons/spoofcookie/SpoofCookieAssignment.java
+++ src/main/java/org/owasp/webgoat/lessons/spoofcookie/SpoofCookieAssignment.java
@@ -73,6 +73,7 @@ public class SpoofCookieAssignment implements AssignmentEndpoint {
     if (!authPassword.isBlank() && authPassword.equals(password)) {
       String newCookieValue = EncDec.encode(lowerCasedUsername);
       Cookie newCookie = new Cookie(COOKIE_NAME, newCookieValue);
+      newCookie.setHttpOnly(true);
       newCookie.setPath("/WebGoat");
       newCookie.setSecure(true);
       response.addCookie(newCookie);
