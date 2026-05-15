aed0e714421241d9e981f29b681d9582da4e13a5
Mobb autofixer <git@mobb.ai>
2026-05-15 12:00:32 +0000

    mobb fix commit: a40300d5-401c-4721-978e-b6d2373011b0

--- src/main/resources/webgoat/static/js/goatApp/support/GoatUtils.js
+++ src/main/resources/webgoat/static/js/goatApp/support/GoatUtils.js
@@ -45,7 +45,7 @@ define(['jquery',
                 displayButton: function(id, show) {
                     if ($('#' + id)) {
                         if (show) {
-                            $('#' + id).show();
+                            $("html").find('#' + id).show();
                         } else {
 
                         }
